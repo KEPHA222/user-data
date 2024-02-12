092fddf405ad2a905c3589ba297e7149f7caa116
kepha222 <kephamotari1@gmail.com>
2024-02-12 10:05:20 +0300

    Added 404 - Page not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { FaSadTear } from "react-icons/fa";
+
+const CustomError = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-60">
+        <div className=" inline-flex flex-row">
+          <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
+          <FaSadTear size={30} className="mt-1 ml-2" />
+        </div>
+        <p className="text-lg mb-8">
+          Oops! The requested URL was not found on the Server.
+        </p>
+        <Link href="/">
+          <p className=" text-[#EA5C0B] underline hover:text-gray-900">
+            Go back to the HOMEPAGE
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default CustomError;
