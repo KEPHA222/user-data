9b1c16bcce62d0ea8c6c6a2490bde7b2c1f5137f
kepha222 <kephamotari1@gmail.com>
2024-02-12 11:01:36 +0300

    Added favicon svg for SEO.

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,9 @@ export const metadata = {
   title: "Resources",
   description:
     "Discover & Share Your User Data. JSONPlaceholder is a free online REST API that you can use whenever you need some fake data. It can be in a README on GitHub, for a demo on CodeSandbox, in code examples on Stack Overflow, or simply to test things locally.",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
 };
 
 const RootLayout = ({ children }) => {
